fbd8a6c0b1ce61e22ec5c64fe935dc2f95fb45b7
Youngung Han <hyulives@gmail.com>
2021-06-12 17:54:38 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -37,6 +37,6 @@ class TranslationDataset(Dataset):
 
         #pdb.set_trace()
         model_inputs["labels"] = labels["input_ids"]
-        print(model_inputs)
+        print("model inputs:", model_inputs)
 
         return model_inputs
