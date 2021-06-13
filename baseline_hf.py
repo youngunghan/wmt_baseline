39cd5b36980b556e03b28f04f08028faf10cf3ba
Youngung Han <hyulives@gmail.com>
2021-06-13 15:47:44 +0900

    Update baseline_hf.py

--- baseline_hf.py
+++ baseline_hf.py
@@ -252,16 +252,17 @@ def main():
     
     en_data = [line.rstrip('\n').split('\t')[0] for line in open(Path(data_args.data_path) / 'train.tsv', 'r').readlines()]
     fr_data = [line.rstrip('\n').split('\t')[1] for line in open(Path(data_args.data_path) / 'train.tsv', 'r').readlines()]
-    '''
+    
     en_aug_data = translate_fr_to_en(fr_data)
     fr_aug_data = translate_en_to_fr(en_data)
     print("en_aug: ", en_aug_data)
     print("fr_aug: ", fr_aug_data)
+    
     '''
     print("en_data:", en_data)
     print("len_endata", len(en_data))
     print("endata[0]", en_data[0])
-
+    '''
     train_data = [line.rstrip('\n').split('\t') for line in open(Path(data_args.data_path) / 'train.tsv', 'r').readlines()]
     dev_data = [line.rstrip('\n').split('\t') for line in open(Path(data_args.data_path) / 'dev.tsv', 'r').readlines()]
     test_data = [line.rstrip('\n').split('\t') for line in open(Path(data_args.data_path) / 'test.tsv', 'r').readlines()]
